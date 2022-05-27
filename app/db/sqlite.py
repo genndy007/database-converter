b511b0ea72ed564de542534bef02db9d396657c9
genndy007 <kochevgenazp@gmail.com>
2022-05-27 23:13:18 +0300

    Automate db filling with python

--- app/db/sqlite.py
+++ app/db/sqlite.py
@@ -1,6 +1,7 @@
 import sqlite3
 from app.db import SQL_DIR, CACHE_DIR
 
+DB_INIT_SCRIPT = 'init_hw.sql'
 
 class SQLite:
     def __init__(self, file=f'{CACHE_DIR}/db.sqlite3'):
@@ -18,5 +19,8 @@ class SQLite:
 
 def initialize_hw_database():
     with SQLite() as cur:
-        with open(f'{SQL_DIR}/init_hw.sql') as sql:
-            cur.execute(sql.read())
+        with open(f'{SQL_DIR}/{DB_INIT_SCRIPT}') as sql:
+            cur.executescript(sql.read())
+
+        for row in cur.execute('select * from purchases'):
+            print(row)
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from app.db.sqlite import SQLite
+from app.db.sqlite import SQLite, initialize_hw_database
 
-with SQLite() as cur:
-    for row in cur.execute('select sqlite_version();'):
-        print(row)
+initialize_hw_database()
