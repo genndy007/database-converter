ba296c34543aca7dc6a5fa1348387cf49f9908dc
genndy007 <kochevgenazp@gmail.com>
2022-05-29 22:39:22 +0300

    Add tablenames list

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,4 +1,6 @@
 import os
+
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 SQL_DIR = f'{THIS_DIR}/sql/'
 CACHE_DIR = f'{THIS_DIR}/cache/'
+TABLE_NAMES = ['customers', 'suppliers', 'hardwares', 'purchases']
