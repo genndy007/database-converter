0dd7c51eefb964b0bf34697dde6a7b88f93d39e1
genndy007 <kochevgenazp@gmail.com>
2022-05-29 20:33:29 +0300

    Add postgres init automation

--- app/db/postgres.py
+++ app/db/postgres.py
@@ -1,6 +1,8 @@
 import psycopg2
 from app.db.config import POSTGRES_CONNECTION_DATA
+from app.db import SQL_DIR
 
+DB_INIT_SCRIPT = 'init_hw_postgre.sql'
 
 class Postgres:
     def __enter__(self):
@@ -10,3 +12,8 @@ class Postgres:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.commit()
         self.conn.close()
+
+def init_hw_pg_db():
+    with Postgres() as cur:
+        with open(f'{SQL_DIR}/{DB_INIT_SCRIPT}') as sql:
+            cur.execute(sql.read())
