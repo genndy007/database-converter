7132c5faa3f003c4b2db1d47ab582061b4cdd1c8
genndy007 <kochevgenazp@gmail.com>
2022-05-30 17:14:19 +0300

    Fix mysql autocommit

--- app/db/mysql.py
+++ app/db/mysql.py
@@ -1,14 +1,20 @@
 import mysql.connector
 from app.db.config import MYSQL_CONNECTION_DATA
 from app.db.config import DB_NAME
+from app.db import SQL_DIR
+
+DB_INIT_SCRIPT = 'init_hw_my.sql'
+
 
 class MySQL:
     def __enter__(self):
         self.conn = mysql.connector.connect(**MYSQL_CONNECTION_DATA)
-        return self.conn.cursor()
+        self.conn.autocommit = True
+        self.cursor = self.conn.cursor()
+        return self.cursor
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.conn.commit()
+        self.cursor.close()
         self.conn.close()
 
 
@@ -20,4 +26,7 @@ def recreate_mysql_db():
 
 def init_hw_mysql_db():
     with MySQL() as cur:
-        pass
+        cur.execute(f'use {DB_NAME}')
+        with open(f'{SQL_DIR}/{DB_INIT_SCRIPT}') as sql:
+            cur.execute(sql.read(), multi=True)
+            cur.close()
