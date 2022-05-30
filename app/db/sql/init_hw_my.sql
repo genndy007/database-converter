ed61527399d0771d82d9a8ed6676cd22a22b2dcd
genndy007 <kochevgenazp@gmail.com>
2022-05-30 17:13:41 +0300

    Add table init file for mysql

---
+++ app/db/sql/init_hw_my.sql
@@ -0,0 +1,39 @@
+drop table if exists purchases cascade;
+drop table if exists hardwares cascade;
+drop table if exists suppliers cascade;
+drop table if exists customers cascade;
+
+
+
+
+create table customers (
+    customer_id int auto_increment primary key,
+    full_name text not null,
+    address text not null,
+    email text
+);
+
+create table suppliers (
+    supplier_id int auto_increment primary key,
+    company_name text not null,
+    phone_number text not null
+);
+
+create table hardwares (
+    hardware_id int auto_increment primary key,
+    supplier_id int,
+    item_name text not null,
+    price real not null,
+    amount integer not null,
+    foreign key (supplier_id) references suppliers(supplier_id)
+);
+
+create table purchases (
+    purchase_id int auto_increment primary key,
+    customer_id integer,
+    hardware_id integer,
+    amount integer not null,
+    total_price real not null,
+    foreign key (customer_id) references customers(customer_id),
+    foreign key (hardware_id) references hardwares(hardware_id)
+);
