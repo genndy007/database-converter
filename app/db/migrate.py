b52772e22deb9e8de58853e3aa6afa55e7d80fc4
genndy007 <kochevgenazp@gmail.com>
2022-05-30 17:14:39 +0300

    Create migration to mysql

--- app/db/migrate.py
+++ app/db/migrate.py
@@ -1,6 +1,8 @@
 from app.db.sqlite import SQLite
 from app.db.postgres import Postgres
+from app.db.mysql import MySQL
 from app.db import TABLE_NAMES
+from app.db.config import DB_NAME
 
 
 def migrate_sqlite_to_pg():
@@ -10,3 +12,14 @@ def migrate_sqlite_to_pg():
             for row in rows:
                 pg_cur.execute(f'INSERT INTO {table_name} VALUES {row}')
 
+
+def migrate_pg_to_mysql():
+    with Postgres() as pg_cur, MySQL() as my_cur:
+        my_cur.execute(f'use {DB_NAME}')
+        for table_name in TABLE_NAMES:
+            pg_cur.execute(f'SELECT * FROM {table_name}')
+            rows = pg_cur.fetchall()
+            for row in rows:
+                my_cur.execute(f'INSERT INTO {table_name} VALUES {row}')
+
+        my_cur.close()
