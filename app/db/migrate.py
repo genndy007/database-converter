ff9fc2da05e59251f75971f3a427054e9113e921
genndy007 <kochevgenazp@gmail.com>
2022-06-04 17:32:38 +0300

    Fix migrations between dbs

--- app/db/migrate.py
+++ app/db/migrate.py
@@ -26,16 +26,16 @@ def migrate_sqlite_to_pg():
 
 # only 3 fields from pg, 3 entries to change
 def migrate_pg_to_mysql():
-    init_hw_mysql_db()
     with Postgres() as pg_cur, MySQL() as my_cur:
-        my_cur.execute(f'use {DB_NAME}')
         for table_name in TABLE_NAMES:
             fields = ', '.join(MYSQL_TABLE_FIELDS[table_name])
             pg_cur.execute(f'SELECT {fields} FROM {table_name}')
             rows = pg_cur.fetchall()
+            my_cur.execute(f'SET FOREIGN_KEY_CHECKS=0')
+            my_cur.execute(f'truncate table {table_name}')
+            my_cur.execute(f'SET FOREIGN_KEY_CHECKS=1')
             for row in rows:
                 my_cur.execute(f'INSERT INTO {table_name}({fields}) VALUES {row}')
-
 
         # update rows
         for pk in (1, 2, 3):
@@ -45,4 +45,4 @@ def migrate_pg_to_mysql():
             my_cur.execute(
                 f'''update hardwares set item_name = concat('updated ', item_name) where hardware_id = {pk}''')
 
-        my_cur.close()
+
