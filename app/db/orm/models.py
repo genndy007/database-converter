53a43993eddcf1e75ec7aa10daecf76c1cb3eba2
genndy007 <kochevgenazp@gmail.com>
2022-06-09 20:27:18 +0300

    Create models and db proxy

--- app/db/orm/models.py
+++ app/db/orm/models.py
@@ -1,56 +1,57 @@
 import peewee as pw
-
+from playhouse.mysql_ext import MySQLConnectorDatabase
 from app.db import CACHE_DIR
+from app.db.orm import (
+    POSTGRES_CONNECTION_DATA,
+    MYSQL_CONNECTION_DATA,
+    DB_NAME,
+    SQLITE_DB_FILE_NAME
+)
 
-DB_FILE_NAME = 'hw_storage_orm.sqlite3'
+database_proxy = pw.DatabaseProxy()
 
-db1 = pw.SqliteDatabase(f'{CACHE_DIR}/{DB_FILE_NAME}')
+db1 = pw.SqliteDatabase(f'{CACHE_DIR}/{SQLITE_DB_FILE_NAME}')
+db2 = pw.PostgresqlDatabase(DB_NAME, **POSTGRES_CONNECTION_DATA)
+db3 = MySQLConnectorDatabase(DB_NAME, **MYSQL_CONNECTION_DATA)
 
 
-class Customer(pw.Model):
+class BaseModel(pw.Model):
+    class Meta:
+        database = database_proxy
+
+
+class Customer(BaseModel):
     full_name = pw.CharField()
     address = pw.CharField()
     email = pw.CharField()
 
-    class Meta:
-        database = db1
-        table_name = 'customers'
 
-
-class Supplier(pw.Model):
+class Supplier(BaseModel):
     company_name = pw.CharField()
     phone_number = pw.CharField()
 
-    class Meta:
-        database = db1
-        table_name = 'suppliers'
 
-
-class Hardware(pw.Model):
+class Hardware(BaseModel):
     supplier = pw.ForeignKeyField(Supplier)
     item_name = pw.CharField()
     price = pw.FloatField()
     amount = pw.IntegerField()
 
-    class Meta:
-        database = db1
-        table_name = 'hardwares'
 
-
-class Purchase(pw.Model):
+class Purchase(BaseModel):
     customer = pw.ForeignKeyField(Customer)
     hardware = pw.ForeignKeyField(Hardware)
     amount = pw.IntegerField()
     total_price = pw.FloatField()
 
-    class Meta:
-        database = db1
-        table_name = 'purchases'
+
+models_list = [Customer, Supplier, Hardware, Purchase]
 
 
 def init_hw_db_orm():
+    database_proxy.initialize(db1)
     db1.connect()
-    db1.create_tables([Customer, Supplier, Hardware, Purchase])
+    db1.create_tables(models_list)
 
     for letter in 'abcde':
         Customer.create(full_name=f'{letter * 3}',
@@ -69,3 +70,17 @@ def init_hw_db_orm():
                         hardware=hardware,
                         amount=2,
                         total_price=hardware.price * 2)
+
+
+def get_all_customers_dicts(db: pw.SqliteDatabase | pw.PostgresqlDatabase | MySQLConnectorDatabase = db1):
+    database_proxy.initialize(db)
+    db.connect(reuse_if_open=True)
+
+    return [row for row in Customer.select().dicts()]
+
+
+
+
+
+
+
